2f8b6d75c5754912d1f8353a591ab5bcc7d49364
D. Wang <david.wang@Hotmail.ca>
2017-09-16 02:41:26 -0400

    String to JSON

---
+++ basic_arg_parser.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 02:17:00 2017
+
+@author: david
+"""
+
+import simplejson as json
+
+def tester(string):
+    stringJSON = json.loads(string)
+    return stringJSON
