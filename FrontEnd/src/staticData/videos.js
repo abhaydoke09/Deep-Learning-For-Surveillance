6fc56851779888290d2e083da2aaa56bcb2be22b
dan12t3 <dan12t3@gmail.com>
2017-09-17 04:16:53 -0400

    finally done now

--- FrontEnd/src/staticData/videos.js
+++ FrontEnd/src/staticData/videos.js
@@ -1,13 +1,16 @@
+import thumb1 from '../data/thumbnail1.png';
+import thumb2 from '../data/thumbnail2.png';
+
 const videos = [
   {
     videoUrl: 'https://youtu.be/fjOu74uHgwo',
-    thumbnailUrl: 'http://yougetmouth.com/wp-content/uploads/2015/07/video.png',
+    thumbnailUrl: thumb2,
     fileName: 'fileOne',
     title: 'Unattended Bag'
   },
   {
     videoUrl: 'https://youtu.be/oS80hlsNvHo',
-    thumbnailUrl: 'http://yougetmouth.com/wp-content/uploads/2015/07/video.png',
+    thumbnailUrl: thumb1,
     fileName: 'fileTwo',
     title: 'Virat Data Set'
   }
